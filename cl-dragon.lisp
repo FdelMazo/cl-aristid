8f52f859b533db06001097a259acca118a9dee6f
JDSanto <javidisanto@gmail.com>
2020-05-23 23:32:18 -0300

    Dynamic array dimensions

--- cl-dragon.lisp
+++ cl-dragon.lisp
@@ -2,8 +2,10 @@
 
 (in-package #:cl-dragon)
 
-(defun dragon-arr ()
-  make-array '(2 3) :initial-contents '((0 1 0) (0 1 0) (0 1 0)))
+(defun dragon-arr (n m)
+  (let ((array (make-array (list n m) :initial-element 1)))
+    (setf (aref array 1 2) 0) array))
 
 (defun dragon (n) (
-  netpbm:write-to-file (format nil "dragon_~3,'0d.pbm" n) (dragon-arr) :format :pbm :encoding :ascii))
+  netpbm:write-to-file (format nil "dragon_~3,'0d.pbm" n) (dragon-arr 50 50) :format :pbm :encoding :ascii :if-exists :overwrite))
+
