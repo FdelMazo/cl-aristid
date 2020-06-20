0116f623f2c929954ec02cb5530d2fe9d52c1eac
FdelMazo <fdelmazo@fi.uba.ar>
2020-06-20 15:33:01 -0300

    Changes color behaviour

--- cl-aristid.lisp
+++ cl-aristid.lisp
@@ -1,6 +1,6 @@
 (in-package #:cl-aristid)
 
-(setf *random-state* (make-random-state t))
+(setq *random-state* (make-random-state t))
 
 (defun -> (old new &optional prob)
   #'(lambda (seq)
@@ -11,53 +11,26 @@
 (defmacro defrule (sym -> replace &key (prob 1.00))
   `(-> ',sym ',replace ,prob))
 
-(defun aristid (&key (angle 0) (len 0) (nodraw nil) (color "black"))
+(defun aristid (&key (angle 0) (len 0) (nodraw nil) (color ""))
   #'(lambda (canvas)
       (dotimes (n len)
-        (setf canvas (canvas-move canvas))
+        (setq canvas (canvas-move canvas))
         (if (null nodraw)
           (draw-point canvas color)))
-      (setf canvas (turn-angle canvas angle))
+      (setq canvas (turn-angle canvas angle))
       canvas))
-
-(defun [ (canvas)
-  (setf canvas (push-stack canvas))
-  canvas)
-
-(defun ] (canvas)
-  (setf canvas (pop-stack canvas))
-  canvas)
 
 (defmacro defaristid (name &rest body)
   `(defun ,name (canvas)
     (funcall (aristid ,@body) canvas)))
 
-(defun flatten (tree)
-  (loop for e in tree
-        nconc
-        (if (consp e)
-            (copy-list e)
-          (list e))))
+(defun [ (canvas)
+  (setq canvas (push-stack canvas)))
 
-(defun string-rewrite (str rules)
-  (flatten
-      (loop :for r :in rules :with seq := str
-        :do (setq seq (funcall r seq))
-        :finally (return seq))))
-
-(defun commands (n str rules)
-  (if (= n 0) (return-from commands str))
-  (commands
-    (1- n)
-    (string-rewrite str rules)
-    rules))
-
-(defun apply-commands (canvas command-arr)
-    (loop :for c :in command-arr :with seq := canvas
-        :do (if (fboundp c) (setq seq (funcall c seq)))
-        :finally (return seq)))
+(defun ] (canvas)
+  (setq canvas (pop-stack canvas)))
 
 (defun draw (fractal gen)
-  (with-open-file (s (format nil "~A_~3,'0d.svg" (fractal-name fractal) gen)
+  (with-open-file (f (format nil "~A_~3,'0d.svg" (fractal-name fractal) gen)
                   :direction :output :if-exists :supersede)
-    (cl-svg:stream-out s (draw-fractal fractal gen))))
+    (cl-svg:stream-out f (draw-fractal fractal gen))))
--- fractal.lisp
+++ fractal.lisp
@@ -5,6 +5,30 @@
   axiom
   rules)
 
+(defun flatten (tree)
+  (loop :for e :in tree :append
+        (if (consp e) (copy-list e)
+          (list e))))
+
+(defun string-rewrite (str rules)
+  (flatten
+      (loop :for r :in rules :with seq := str
+        :do (setq seq (funcall r seq))
+        :finally (return seq))))
+
+(defun commands (n str rules)
+  (if (= n 0) (return-from commands str))
+  (commands (1- n) (string-rewrite str rules)  rules))
+
+(defun apply-commands (canvas command-arr)
+    (loop :for c :in command-arr :with seq := canvas
+        :do (if (fboundp c) (setq seq (funcall c seq)))
+        :finally (return seq)))
+
 (defun draw-fractal (fractal gen)
-  (let ((command-arr (commands gen (fractal-axiom fractal) (fractal-rules fractal))))
-    (create-svg (apply-commands (make-canvas) command-arr))))
+  (let* ((command-arr (commands gen (fractal-axiom fractal)
+                                   (fractal-rules fractal)))
+         (canvas (make-canvas))
+         (svg (create-svg (apply-commands canvas command-arr))))
+    (svg-add-iteration svg canvas)
+    svg))
